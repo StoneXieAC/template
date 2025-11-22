ae60c3b63cbd5bc4c22c4017d1f438336f07428c
StoneXieAC <2323604986@qq.com>
2025-11-22 20:59:19 +0800

    update

--- 封装模板/模数类.cpp
+++ 封装模板/模数类.cpp
@@ -1,167 +1,104 @@
-template<const long long MOD>
+template<int MOD>
 struct ModNum {
-    using integer = long long;
-    integer x;
+    int x;
 
-    ModNum() : x(0ll) {}
+    ModNum() : x(0) {}
 
-    explicit ModNum(integer y) {
-        if (0 <= y && y < MOD) x = y;
-        else {
-            x = y % MOD;
-            if (x < 0) x += MOD;
-        }
+    template<class T>
+    static int norm(T v) {
+        v %= MOD;
+        if (v < 0) v += MOD;
+        return (int) v;
     }
 
-    integer Int() { return x; }
+    template<class T>
+    ModNum(T v) {
+        x = norm(v);
+    }
 
-    ModNum Pow(integer y) {
-        integer res = 1, tmp = x;
-        while (y) {
-            if (y & 1) res = res * tmp % MOD;
-            y >>= 1;
-            tmp = tmp * tmp % MOD;
+
+    int val() const { return x; }
+
+    template<class T>
+    ModNum pow(T e) const {
+        int base = x, res = 1;
+        while (e) {
+            if (e & 1) res = 1LL * res * base % MOD;
+            base = 1LL * base * base % MOD;
+            e >>= 1;
         }
         return ModNum(res);
     }
 
-    ModNum &operator=(integer y) {
-        if (0 <= y && y < MOD) x = y;
-        else {
-            x = y % MOD;
-            if (x < 0) x += MOD;
-        }
+
+    ModNum operator+(const ModNum &other) const {
+        int t = x + other.x;
+        if (t >= MOD) t -= MOD;
+        return ModNum(t);
+    }
+
+    ModNum operator-(const ModNum &other) const {
+        int t = x - other.x;
+        if (t < 0) t += MOD;
+        return ModNum(t);
+    }
+
+    ModNum operator*(const ModNum &other) const {
+        return ModNum(1LL * x * other.x % MOD);
+    }
+
+    ModNum inv() const {
+        return pow(MOD - 2);
+    }
+
+    ModNum operator/(const ModNum &other) const {
+        return *this * other.inv();
+    }
+
+    ModNum &operator+=(const ModNum &other) { return *this = *this + other; }
+
+    ModNum &operator-=(const ModNum &other) { return *this = *this - other; }
+
+    ModNum &operator*=(const ModNum &other) { return *this = *this * other; }
+
+    ModNum &operator/=(const ModNum &other) { return *this = *this / other; }
+
+    bool operator==(const ModNum &other) const { return x == other.x; }
+
+    bool operator!=(const ModNum &other) const { return x != other.x; }
+
+    ModNum &operator++() {
+        x++;
+        if (x == MOD) x = 0;
         return *this;
     }
 
-    bool operator==(integer y) const { return x == y; }
-
-    bool operator==(ModNum y) const { return x == y.x; }
-
-    bool operator!=(integer y) const { return x != y; }
-
-    bool operator!=(ModNum y) const { return x != y.x; }
-
-    bool operator<(integer y) const { return x < y; }
-
-    bool operator<(ModNum y) const { return x < y.x; }
-
-    bool operator>(integer y) const { return x > y; }
-
-    bool operator>(ModNum y) const { return x > y.x; }
-
-    bool operator<=(integer y) const { return x <= y; }
-
-    bool operator<=(ModNum y) const { return x <= y.x; }
-
-    bool operator>=(integer y) const { return x >= y; }
-
-    bool operator>=(ModNum y) const { return x >= y.x; }
-
-    ModNum operator+(ModNum y) const {
-        ModNum res;
-        if (x + y.x < MOD) res.x = x + y.x;
-        else res.x = x + y.x - MOD;
-        return res;
-    }
-
-    ModNum operator-(ModNum y) const {
-        ModNum res;
-        if (x - y.x >= 0) res.x = x - y.x;
-        else res.x = x - y.x + MOD;
-        return res;
-    }
-
-    ModNum operator*(ModNum y) const {
-        ModNum res;
-        res.x = x * y.x % MOD;
-        if (res.x < 0) res.x += MOD;
-        return res;
-    }
-
-    ModNum operator/(ModNum y) const {
-        ModNum res;
-        res.x = x * y.Pow(MOD - 2).x % MOD;
-        if (res.x < 0) res.x += MOD;
-        return res;
-    }
-
-    ModNum operator+(integer y) const { return *this + ModNum(y); }
-
-    ModNum operator-(integer y) const { return *this - ModNum(y); }
-
-    ModNum operator*(integer y) const { return *this * ModNum(y); }
-
-    ModNum operator/(integer y) const { return *this / ModNum(y); }
-
-    void operator+=(ModNum y) { *this = *this + y; }
-
-    void operator*=(ModNum y) { *this = *this * y; }
-
-    void operator-=(ModNum y) { *this = *this - y; }
-
-    void operator/=(ModNum y) { *this = *this / y; }
-
-    void operator+=(integer y) { *this = *this + ModNum(y); }
-
-    void operator-=(integer y) { *this = *this - ModNum(y); }
-
-    void operator*=(integer y) { *this = *this * ModNum(y); }
-
-    void operator/=(integer y) { *this = *this / ModNum(y); }
-
-    ModNum operator^(integer y) { return Pow(y); }
-
-    template<class T>
-    friend bool operator==(T y, ModNum t) { return y == t.x; }
-
-    template<class T>
-    friend ModNum operator+(T t, ModNum y) { return ModNum(t) + y; }
-
-    template<class T>
-    friend ModNum operator*(T t, ModNum y) { return ModNum(t) * y; }
-
-    template<class T>
-    friend ModNum operator-(T t, ModNum y) { return ModNum(t) - y; }
-
-    template<class T>
-    friend ModNum operator/(T t, ModNum y) { return ModNum(t) / y; }
-
-    void operator++() {
-        ++x;
-        if (x == MOD) x = 0;
-    }
-
-    void operator--() {
+    ModNum &operator--() {
         if (x == 0) x = MOD;
-        --x;
+        x--;
+        return *this;
     }
 
     ModNum operator++(signed) {
-        ModNum tmp = *this;
-        ++x;
-        if (x == MOD) x = 0;
-        return tmp;
+        ModNum t = *this;
+        ++*this;
+        return t;
     }
 
     ModNum operator--(signed) {
-        ModNum tmp = *this;
-        if (x == 0) x = MOD;
-        --x;
-        return tmp;
+        ModNum t = *this;
+        --*this;
+        return t;
     }
 
-    friend std::ostream &operator<<(std::ostream &os, const ModNum &obj) {
-        os << obj.x;
-        return os;
+    friend std::ostream &operator<<(std::ostream &os, const ModNum &v) {
+        return os << v.x;
     }
 
-    friend std::istream &operator>>(std::istream &is, ModNum &obj) {
-        is >> obj.x;
-        if (obj.x < 0 || obj.x >= MOD) {
-            obj.x = obj.x % MOD;
-            if (obj.x < 0) obj.x += MOD;
-        }
+    friend std::istream &operator>>(std::istream &is, ModNum &v) {
+        long long y;
+        is >> y;
+        v.x = norm(y);
         return is;
     }
 };
@@ -170,10 +107,10 @@ const long long M = 998244353;
 using mint = ModNum<M>;
 
 std::array<std::vector<mint>, 2> getfac(int siz) {
-    std::vector <mint> fac(siz + 1), inv(siz + 1);
+    std::vector<mint> fac(siz + 1), inv(siz + 1);
     fac[0] = inv[0] = 1;
     for (int i = 1; i <= siz; i++) fac[i] = fac[i - 1] * i;
-    inv[siz] = 1 / fac[siz];
+    inv[siz] = mint(1) / fac[siz];
     for (int i = siz - 1; i >= 1; i--) inv[i] = inv[i + 1] * (i + 1);
     return {fac, inv};
 }
